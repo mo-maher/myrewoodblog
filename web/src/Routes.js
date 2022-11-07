365520cfa45ff4cecf791f248a60c49e987b8963
steelhelmet1 <egyman2010@gmail.com>
2022-11-07 16:52:09 +0200

    first commit

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,10 +1,10 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
+// In this file, all Page components from 'src/pages` are auto-imported.Nested
 // directories are supported, and should be uppercase. Each subdirectory will be
 // prepended onto the component name.
 //
 // Examples:
 //
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
+// 'src/pages/HomePage/HomePage.js'  -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 import { Private, Router, Route, Set } from '@redwoodjs/router'
 
